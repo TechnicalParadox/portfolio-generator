0cd026893f685efb7948e93121e107eaf64ce6a6
Giamo Lao <giamolao@paradoxicalsw.com>
2020-12-17 05:30:45 -0500

    output html

--- app.js
+++ app.js
@@ -13,8 +13,8 @@
 // printProfileData(profileDataArgs);
 
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 //
 // let pageHTML = generatePage(name, github);
 //
@@ -113,4 +113,13 @@ inquirer
     }
   ])
   .then(promptProject)
-  .then(portfolioData => console.log(portfolioData));
+  .then(portfolioData =>
+  {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
